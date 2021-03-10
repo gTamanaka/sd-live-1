7d07d1319e5b40209723dfca735f101fde7c8c82
Gustavo Tamanaka <gustavo@sflabs.com.br>
2021-03-10 17:01:00 -0300

    [WIP] Code explanation

--- only-consumer/app.js
+++ only-consumer/app.js
@@ -1,7 +1,8 @@
-const { Kafka } = require('kafkajs')
+const { Kafka } = require('kafkajs') //Importar as biblioteca do kafka
+
 const kafka = new Kafka({
   clientId: 'only-consumer',
-  brokers: ['localhost:29092']
+  brokers: ['localhost:29092'] //Espeficicar o endereço onde está o kafka
 })
 
 const start = async () =>{
@@ -10,11 +11,11 @@ const start = async () =>{
         const consumer = kafka.consumer({ groupId: 'producer' })
         
         await consumer.connect()
-        await consumer.subscribe({ topic: 'test-topic'})
+        await consumer.subscribe({ topic: 'test-topic'}) // Escolha o tópico que você quer ouvir
         
         await consumer.run({
             eachMessage: async ({ topic, partition, message }) => {
-                console.log(message.value.toString())
+                console.log(message.value.toString()) // Exibe as mensagens disponíveis no tópico
             },
         })
         console.log("Listen")
--- only-producer/app.py
+++ only-producer/app.py
@@ -1,10 +1,11 @@
-from kafka import KafkaProducer
-import json
-import random
-import time
-from datetime import datetime
+# Este código está cheio de comentários para efeitos de aprendizado
+# quando for fazer código para via real não é preciso
 
-# Create an instance of the Kafka producer
+from kafka import KafkaProducer #Importa a biblioteca para se comunicar com o Kafka
+import time #Importa a biblioteca que contém 
+from datetime import datetime #Importa a biblioteca 
+
+# O código abaixo
 producer = False
 while (not producer):
     try:
@@ -14,7 +15,6 @@ while (not producer):
         print(e)
         time.sleep(20)
 
-# Call the producer.send method with a producer-record
 print("Only Producer UP")
 while True:
     horario = datetime.now()
