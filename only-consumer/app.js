05f9976a5aee39dc6ba6626483726dd46404943c
gTamanaka <tamanaka.gustavo@gmail.com>
2021-03-10 17:10:48 -0300

    Add code explanation for only-producer and only-consumer

--- only-consumer/app.js
+++ only-consumer/app.js
@@ -1,27 +1,31 @@
 const { Kafka } = require('kafkajs') //Importar as biblioteca do kafka
 
+// Criar um novo Objeto para comenicação do Kafka
 const kafka = new Kafka({
   clientId: 'only-consumer',
   brokers: ['localhost:29092'] //Espeficicar o endereço onde está o kafka
 })
 
+// Define a função start que tem por função  criar um consumidor para o tópico test-topic
 const start = async () =>{
     try {
-        
         const consumer = kafka.consumer({ groupId: 'producer' })
         
         await consumer.connect()
         await consumer.subscribe({ topic: 'test-topic'}) // Escolha o tópico que você quer ouvir
         
+        console.log("Listen")
+        // O método abaixo vai ficar ouvindo constantemente as novas mensagens chegando.
         await consumer.run({
             eachMessage: async ({ topic, partition, message }) => {
-                console.log(message.value.toString()) // Exibe as mensagens disponíveis no tópico
+                console.log(message.value.toString()) // Exibe as mensagens disponíveis no tópico.
             },
         })
-        console.log("Listen")
     } catch (error) {
+        // Se falhar, tenta de novo reconnectar. Está não é a melhor solução, mas era a mais fácil de explicar
         start()
     }
 }
 
+// Invoca  a função start que foi criada ali em cima
 start()
--- only-producer/app.py
+++ only-producer/app.py
@@ -1,23 +1,30 @@
 # Este código está cheio de comentários para efeitos de aprendizado
 # quando for fazer código para via real não é preciso
+# Este método é apenas paras fins didáticos.
 
 from kafka import KafkaProducer #Importa a biblioteca para se comunicar com o Kafka
 import time #Importa a biblioteca que contém 
 from datetime import datetime #Importa a biblioteca 
 
-# O código abaixo
+# O código abaixo fica tentando reconectar até que O kafka esteja funcionando
 producer = False
 while (not producer):
+    print("Tentando conectar")
     try:
-        print("Tentando conectar")
+        #bootstrap_servers é o endereço do servidor do kafka
+        #value_serializer é uma função que transforma a entrada value do send() em bytes
         producer = KafkaProducer(bootstrap_servers='localhost:29092',value_serializer=lambda v: str(v).encode('utf-8'))
     except Exception as e:
+        #Se der algum erro printa o erro e espera 20 segundos para tentar reconectar
         print(e)
         time.sleep(20)
 
+#Conectou e estamos prontos para enviar mensagem
 print("Only Producer UP")
+#O loop abaixo envia para o topico test-topic uma data e hora.
 while True:
     horario = datetime.now()
     print(horario)
     producer.send('test-topic', horario)
+    #Aguarda 10 segundo para enviar novamente a mensagem.
     time.sleep(10)
