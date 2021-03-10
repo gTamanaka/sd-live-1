ea2dfbe697ed192a7239f9067cbf6656055ced1d
gTamanaka <tamanaka.gustavo@gmail.com>
2021-03-10 17:49:23 -0300

    Add comments for fake sensors

--- sensor-nivel-agua/app.js
+++ sensor-nivel-agua/app.js
@@ -4,6 +4,7 @@ const kafka = new Kafka({
   brokers: ["localhost:29092"],
 });
 
+// Define a variável de nome do sensor utilizar uma variável de ambiente
 const sensorName = process.env.SENSOR_NAME
 
 const producer = async (leitura) => {
@@ -13,14 +14,18 @@ const producer = async (leitura) => {
     topic: "sensor",
     messages: [
       {
-        value: leitura,
-        timestamp: Date.now(),
-        key: sensorName
+        value: leitura, // O valor da leitura
+        timestamp: Date.now(), // Representa o horário que a leitura foi coletada
+        key: sensorName // Representa o sensor que coletou a leitura, por facilidade coloquei aqui,
+                        // mas poderia estarno header
       },
     ],
   });
 };
 
+// Invoca a cada 10000 ms (10s) um envio de valor aleatório entre 0 e 1 representando 
+// o volume de água do reservatório em %
+// Em uma aplicaćão real seria onde coletáriamos os dados de um sensor.
 setInterval(() => {
   let nivel = Math.random();
   producer(nivel.toString());
--- sensor-temperatura/app.py
+++ sensor-temperatura/app.py
@@ -6,14 +6,14 @@ from datetime import datetime
 import os
 
 
-# Create an instance of the Kafka producer
 producer = KafkaProducer(bootstrap_servers='localhost:29092')
 
-# Call the producer.send method with a producer-record
+# Recupera o nome do sensor como sendo uma variável de ambiente
 print(os.environ["SENSOR_NAME"] + " UP")
+
 while True:
-    reading =  random.randint(24,32) #
-    reading =  bytes( str(reading), 'utf-8')
-    sensor_name=  bytes(os.environ["SENSOR_NAME"], 'utf-8')
-    producer.send('sensor', key = sensor_name, value = reading)
-    time.sleep(60)
+    reading =  random.randint(24,32) # Aqui estamos simulando a leitura do sensor de temperatura
+    reading =  bytes( str(reading), 'utf-8') # Transforma a leitura em bytes para poder enviar, poderia ter feito tudo em uma linha
+    sensor_name=  bytes(os.environ["SENSOR_NAME"], 'utf-8') #Define o nome do sensor pela variável de ambiente
+    producer.send('sensor', key = sensor_name, value = reading) # Envia a leiura de mentira
+    time.sleep(60) #Espera 60 segundos para enviar de novo
